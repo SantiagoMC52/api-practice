43baada165e8746be52c9292ad1834bd5104de0f
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-06-28 14:54:44 +0200

    Add example test of App

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,12 @@
 /* eslint-disable react/jsx-filename-extension */
+import { render } from '@testing-library/react';
 import React from 'react';
-import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('api practice app', () => {
+  it('has a list characters including Rick and Morty', () => {
+    const { getByText } = render(<App />);
+
+    expect(getByText(/Rick and Morty\b/i)).toBeInTheDocument();
+  });
 });
