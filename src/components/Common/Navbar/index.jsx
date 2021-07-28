07f2b32b5c0fa9cba2e60efe387a5b288d6f96d4
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-07-28 16:24:46 +0200

    Refactor style of dashboard and navbar

--- src/components/Common/Navbar/index.jsx
+++ src/components/Common/Navbar/index.jsx
@@ -1,13 +1,16 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './navbar.scss';
 
 const Navbar = () => (
-  <nav className="navbar">
-    <Link to={`/${'character'}`}>Characters</Link>
-    <Link to={`/${'location'}`}>Locations</Link>
-    <Link to={`/${'episode'}`}>Episodes</Link>
-  </nav>
+  <div className="navbar">
+    <nav className="navbar__items">
+      <Link to={`/${'character'}`}>Characters</Link>
+      <Link to={`/${'location'}`}>Locations</Link>
+      <Link to={`/${'episode'}`}>Episodes</Link>
+    </nav>
+  </div>
 );
 
 export default Navbar;
--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import Navbar from '../Common/Navbar';
+import './dashboard.scss';
 
 const Dashboard = () => (
   <>
     <h2 className="dashboard-title">
       Rick and Morty
     </h2>
-    <div className="navbar-container">
-      <Navbar />
-    </div>
+    <Navbar />
   </>
 );
 
